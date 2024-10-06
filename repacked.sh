fefb610830972e6509f17f928496985948e7a4e4
Sinclair732 <91702895+Sinclair732@users.noreply.github.com>
2024-10-06 11:05:49 +0200

    update build path

--- repacked.sh
+++ repacked.sh
@@ -1,34 +1,34 @@
 #!/bin/bash -x
 
 dist="bookworm"
-repacked_path="tmp/repacked"
-mkdir ${repacked}
+repacked_path="build/repacked"
+mkdir -p ${repacked}
 mirror_path="/var/spool/apt-mirror/mirror/download.proxmox.com/debian/devel/dists/${dist}/main/binary-amd64/"
 #newpkg
-ls  $mirror_path |grep librust>tmp/newpkg
+ls  $mirror_path |grep librust > build/newpkg
 #oldpkg
-ls $repacked_path >tmp/oldpkg
+ls $repacked_path > build/oldpkg
 #oldpkg-arm64.deb to amd64.deb
-sed -i "s/arm64/amd64/g" tmp/oldpkg 
+sed -i "s/arm64/amd64/g" build/oldpkg 
 #diff
-diff -u tmp/oldpkg tmp/newpkg |grep +librust|sed "s/^.//g" > tmp/needpkg
+diff -u build/oldpkg build/newpkg |grep +librust|sed "s/^.//g" > build/needpkg
 echo "$(date "+%Y/%m/%d %H:%M:%S") Needpkgs:" 
-if test -s tmp/needpkg
+if test -s build/needpkg
 then
-cat tmp/needpkg
+cat build/needpkg
 else
 echo "all pkg is up date"
 exit 0
 fi
 
-for packlist in `cat tmp/needpkg`;do
-extract_path="tmp/librust/$packlist/extract" 
+for packlist in `cat build/needpkg`;do
+extract_path="build/librust/$packlist/extract" 
 mkdir $extract_path/DEBIAN -p
 dpkg -X $mirror_path$packlist $extract_path > /dev/null
 dpkg -e $mirror_path$packlist $extract_path/DEBIAN > /dev/null
 sed -i "s/amd64/arm64/g" $extract_path/DEBIAN/control > /dev/null
 dpkg-deb -Zxz -b  $extract_path  $repacked_path  > /dev/null
-echo "$(date "+%Y/%m/%d %H:%M:%S") repacked $packlist done" >>tmp/repacked.log
+echo "$(date "+%Y/%m/%d %H:%M:%S") repacked $packlist done" >> build/repacked.log
 done
 echo "$(date "+%Y/%m/%d %H:%M:%S") all package repacked  done" 
-echo "$(date "+%Y/%m/%d %H:%M:%S") all package repacked  done" >>tmp/repacked.log
+echo "$(date "+%Y/%m/%d %H:%M:%S") all package repacked  done" >> build/repacked.log
